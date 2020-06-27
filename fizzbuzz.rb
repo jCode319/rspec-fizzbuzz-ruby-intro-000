bc3a2ad2ad60d328695f0049d4915f2f777fa00f
Jarrell Jackson <jdjack3@gmail.com>
2020-06-27 14:54:39 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,4 +4,13 @@ def fizzbuzz(int)
   if int % 3 == 0
     "Fizz"
   end
+  if int % 5 == 0
+    "Buzz"
+  end
+  if int % 3 || 5 == 0
+    "FizzBuzz"
+  end
+  if int !% 3 || 5 == 0
+    "nil"
+  end
 end
